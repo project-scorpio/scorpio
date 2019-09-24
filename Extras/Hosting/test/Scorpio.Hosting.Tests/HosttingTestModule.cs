ddb8815c83194fee54bb290d15dd9664f1d960ed
Zidan.Wang <zidan.wang@live.com>
2019-09-24 17:22:14 +0800

    add test projects.

---
+++ Extras/Hosting/test/Scorpio.Hosting.Tests/HosttingTestModule.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Scorpio.Modularity;
+using Microsoft.Extensions.DependencyInjection;
+namespace Scorpio.Hosting.Tests
+{
+    public class HosttingTestModule : ScorpioModule
+    {
+        public override void ConfigureServices(ConfigureServicesContext context)
+        {
+            context.Services.RegisterAssemblyByConvention();
+        }
+    }
+}
