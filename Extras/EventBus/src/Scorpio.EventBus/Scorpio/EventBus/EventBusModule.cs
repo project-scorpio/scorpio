4e40f63f79f094c9a53a32fdd0e5f1684a2ce091
Zidan.wang <zidan.wang@gmail.com>
2020-06-04 08:28:57 +0800

    临时提交。

---
+++ Extras/EventBus/src/Scorpio.EventBus/Scorpio/EventBus/EventBusModule.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Scorpio.Modularity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Scorpio.EventBus
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public sealed class EventBusModule:ScorpioModule
+    {
+        public override void ConfigureServices(ConfigureServicesContext context)
+        {
+            context.Services.RegisterAssemblyByConvention();
+        }
+    }
+}
