26bf022bbd27316481d4d916c0b07f99f61b494e
王子丹 <wangzidan@xmsailina.com>
2022-05-14 16:25:14 +0800

    添加单元测试。

---
+++ src/Core/test/Scorpio.Utilities.Tests/System/NullDisposable_Tests.cs
@@ -0,0 +1,28 @@
+﻿
+using System.Threading.Tasks;
+
+using Shouldly;
+
+using Xunit;
+
+namespace System
+{
+    public class NullDisposable_Tests
+    {
+        [Fact]
+        public void Instance()
+        {
+            var instance = NullDisposable.Instance;
+            Should.NotThrow(() => instance.Dispose());
+        }
+    }
+    public class AsyncNullDisposable_Tests
+    {
+        [Fact]
+        public async ValueTask Instance()
+        {
+            var instance = NullAsyncDispose.Instance;
+          await  Should.NotThrow(() => instance.DisposeAsync());
+        }
+    }
+}
