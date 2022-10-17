841c317a5a8004571d7341e243d80525f48f1007
王子丹 <zidan.wang@live.com>
2022-10-17 10:03:34 +0800

    添加Copy按钮

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,7 +11,22 @@ layout: null
             trigger: 'focus'
         })
     });
+    var allCodeBlocksElements = $( "code" );
 
+    allCodeBlocksElements.each(function(i) {
+         // add different id for each code block
+    
+        // target	
+      var currentId = "codeblock" + (i + 1);
+      $(this).attr('id', currentId);
+         
+      //trigger
+      var clipButton = '<button class="btn" data-clipboard-target="#' + currentId + '"><img src="https://clipboardjs.com/assets/images/clippy.svg" width="13" alt="Copy to clipboard"></button>';
+         $(this).after(clipButton);
+      });
+     
+      new Clipboard('.btn');
+    
     function bottomPos(element) {
         return element.offset().top + element.outerHeight();
     }
