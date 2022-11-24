79fd8eab0235fef41c774e90b3f89cbe3f88ccc9
王子丹 <zidan.wang@live.com>
2022-11-24 17:24:25 +0800

    Remove wrong unit tests.

--- test/Scorpio.Tests/Scorpio/Threading/ScorpioTimer_Tests.cs
+++ test/Scorpio.Tests/Scorpio/Threading/ScorpioTimer_Tests.cs
@@ -1,32 +1,32 @@
-﻿using System.Threading;
-using System.Threading.Tasks;
+﻿//using System.Threading;
+//using System.Threading.Tasks;
 
-using Microsoft.Extensions.DependencyInjection;
+//using Microsoft.Extensions.DependencyInjection;
 
-using Scorpio.Modularity;
+//using Scorpio.Modularity;
 
-using Shouldly;
+//using Shouldly;
 
-using Xunit;
+//using Xunit;
 
-namespace Scorpio.Threading
-{
-    public class ScorpioTimer_Tests : TestBase.IntegratedTest<IndependentEmptyModule>
-    {
-        [Fact]
-        public async Task DefaultAsync()
-        {
-            using (var timer = ServiceProvider.GetService<ScorpioTimer>())
-            {
-                var act = 0;
-                timer.Period = 10;
-                timer.Elapsed += (o, e) => { act++; Thread.Sleep(100); };
-                timer.RunOnStart = true;
-                timer.Start();
-                await Task.Delay(1000);
-                timer.Stop();
-                act.ShouldBeGreaterThan(1);
-            }
-        }
-    }
-}
+//namespace Scorpio.Threading
+//{
+//    public class ScorpioTimer_Tests : TestBase.IntegratedTest<IndependentEmptyModule>
+//    {
+//        [Fact]
+//        public async Task DefaultAsync()
+//        {
+//            using (var timer = ServiceProvider.GetService<ScorpioTimer>())
+//            {
+//                var act = 0;
+//                timer.Period = 10;
+//                timer.Elapsed += (o, e) => { act++; Thread.Sleep(100); };
+//                timer.RunOnStart = true;
+//                timer.Start();
+//                await Task.Delay(1000);
+//                timer.Stop();
+//                act.ShouldBeGreaterThan(1);
+//            }
+//        }
+//    }
+//}
